6827097d66653a4c41515d3ce00bb9f1bdd0d6ea
Bossinc <ajhack93@gmail.com>
2016-04-04 23:32:23 -0500

    Set up tokens

---
+++ src/srrrillumination/Token_State.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package srrrillumination;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author v3b2
+ */
+public class Token_State {
+    ArrayList<Token_State> gotoStates;
+    ArrayList<Token_State> shiftStates;
+}
